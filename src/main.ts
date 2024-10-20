4cece14cc28e8a088ffce783500aa66f6cc0bfb6
jon-edward <townsend.jonathan.e@gmail.com>
2024-10-20 18:28:58 -0400

    Add sections to projects

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
 import "./style.css";
+
+const projectsSection = document.getElementById("projects")!;
+
+const aboutMeSection = document.getElementById("about-me")!;
+
+function openProjectDetails() {
+  const detailsElements = projectsSection.querySelectorAll("details");
+
+  for (const element of detailsElements) {
+    element.setAttribute("open", "true");
+  }
+}
+
+function handleHash(hash: string) {
+  switch (hash) {
+    case "#projects":
+      openProjectDetails();
+      window.scrollTo({
+        top: projectsSection.offsetTop,
+        behavior: "smooth",
+      });
+      break;
+    case "#about-me":
+      window.scrollTo({
+        top: aboutMeSection.offsetTop,
+        behavior: "smooth",
+      });
+      break;
+    default:
+      break;
+  }
+}
+
+handleHash(window.location.hash);
+
+window.onhashchange = (_event) => {
+  handleHash(window.location.hash);
+};
+
+window.onpopstate = (_event) => {
+  handleHash(window.location.href);
+};
