253bf723a9da2e471f7bcdd69be37bb5aabb8d15
jon-edward <townsend.jonathan.e@gmail.com>
2024-10-20 20:03:14 -0400

    Visual changes, start with details open

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,17 @@
 import "./style.css";
 
 const projectsSection = document.getElementById("projects")!;
-
 const aboutMeSection = document.getElementById("about-me")!;
-
-function openProjectDetails() {
-  const detailsElements = projectsSection.querySelectorAll("details");
-
-  for (const element of detailsElements) {
-    element.setAttribute("open", "true");
-  }
-}
-
-function closeProjectDetails() {
-  const detailsElements = projectsSection.querySelectorAll("details");
-
-  for (const element of detailsElements) {
-    element.removeAttribute("open");
-  }
-}
 
 function handleHash(hash: string) {
   switch (hash) {
     case "#projects":
-      openProjectDetails();
       window.scrollTo({
         top: projectsSection.offsetTop,
         behavior: "smooth",
       });
       break;
     case "#about-me":
-      closeProjectDetails();
       window.scrollTo({
         top: aboutMeSection.offsetTop,
         behavior: "smooth",
